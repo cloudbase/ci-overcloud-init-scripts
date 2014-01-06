efd59188db94a5bbc9d3465140dfdd41107d0622
Gabriel Samfira <samfiragabriel@gmail.com>
2014-01-06 14:17:09 +0200

    Added undercloud configs

--- nova_controller/nat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/sbin/iptables -t nat -I POSTROUTING 1 -o eth0 -j MASQUERADE
-/sbin/iptables -I FORWARD 1 -i eth0 -o br100 -m state --state RELATED,ESTABLISHED -j ACCEPT
-/sbin/iptables -I FORWARD 2 -i br100 -o eth0 -j ACCEPT
-
-/sbin/iptables -I INPUT -p tcp -j ACCEPT
-/sbin/iptables -I INPUT -p udp -j ACCEPT
-/sbin/iptables -I INPUT -p icmp -j ACCEPT
