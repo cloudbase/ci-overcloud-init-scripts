1d5fa351de59d38f2f6e219630963624b4fe36e0
Gabriel <samfiragabriel@gmail.com>
2014-01-13 12:52:32 +0200

    pbr install no longer needed

--- scripts/HyperV/scripts/install_openstack_from_repo.bat
+++ scripts/HyperV/scripts/install_openstack_from_repo.bat
@@ -1,6 +1,6 @@
 @echo off
 set PROJ=%1
 
-C:\OpenStack\virtualenv\Scripts\activate.bat && cd %PROJ% && pip install pbr==0.5.22 && python setup.py install
+C:\OpenStack\virtualenv\Scripts\activate.bat && cd %PROJ% && python setup.py install
 
 
