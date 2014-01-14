2e91c227306b6a2c5341dd15e3741d1fb17fcd2d
Gabriel Samfira <samfiragabriel@gmail.com>
2014-01-14 09:32:31 -0500

    Install latest nosetests

--- scripts/devstack_vm/bin/run_tests.sh
+++ scripts/devstack_vm/bin/run_tests.sh
@@ -3,6 +3,7 @@
 cd /opt/stack/tempest/tempest
 
 mkdir -p /home/ubuntu/tempest
+sudo pip install -U nose nose-exclude
 
 nosetests -sv --exclude-test-file=/home/ubuntu/exclude-tests.txt --with-xunit --xunit-file=/home/ubuntu/tempest/result1.xml --detailed-errors tempest.api.compute > /home/ubuntu/tempest/tempest.log 2>&1
 
