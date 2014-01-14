63596a8dfae8564f513dc9c2f84bc929ae87fe6b
Gabriel Samfira <samfiragabriel@gmail.com>
2014-01-14 09:31:22 -0500

    run_tests uses nosetests

--- scripts/devstack_vm/bin/run_tests.sh
+++ scripts/devstack_vm/bin/run_tests.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-cd /opt/stack/tempest
-testr init
-testr run --parallel tempest.api.compute --load-list=/home/ubuntu/tests_clean
+cd /opt/stack/tempest/tempest
+
+mkdir -p /home/ubuntu/tempest
+
+nosetests -sv --exclude-test-file=/home/ubuntu/exclude-tests.txt --with-xunit --xunit-file=/home/ubuntu/tempest/result1.xml --detailed-errors tempest.api.compute > /home/ubuntu/tempest/tempest.log 2>&1
+
+# testr init
+# testr run --parallel tempest.api.compute --load-list=/home/ubuntu/tests_clean
 #testr run tempest.api.compute.admin.test_flavors.FlavorsAdminTestJSON.test_create_flavor_with_int_id
 #testr run tempest.api.compute.admin.test_hypervisor.HypervisorAdminTestXML.test_get_hypervisor_uptime
 
