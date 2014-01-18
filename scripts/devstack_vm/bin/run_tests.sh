2e154f084ff22c1a8025774b4f61c89f8d856018
Gabriel Samfira <samfiragabriel@gmail.com>
2014-01-18 13:30:28 -0500

    use testr instead of nosetests

--- scripts/devstack_vm/bin/run_tests.sh
+++ scripts/devstack_vm/bin/run_tests.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-cd /opt/stack/tempest/tempest
+cd /opt/stack/tempest
+
+testr init
 
 mkdir -p /home/ubuntu/tempest
-sudo pip install -U nose nose-exclude
 
-nosetests -sv --exclude-test-file=/home/ubuntu/exclude-tests.txt --with-xunit --xunit-file=/home/ubuntu/tempest/results.xml --detailed-errors tempest.api.compute > /home/ubuntu/tempest/tempest-output.log 2>&1
+#exclude unsupported tests
+testr list-tests tempest.api.compute | grep -v "rescue\|_uptime\|_console_" > /home/ubuntu/testr.list
 
-#nosetests -sv --exclude-test-file=/home/ubuntu/exclude-tests.txt --with-xunit --xunit-file=/home/ubuntu/tempest/result1.xml --detailed-errors tempest.api.compute.admin.test_aggregates.AggregatesAdminTestJSON > /home/ubuntu/tempest/tempest-output.log 2>&1
-
-# testr init
-# testr run --parallel tempest.api.compute --load-list=/home/ubuntu/tests_clean
-#testr run tempest.api.compute.admin.test_flavors.FlavorsAdminTestJSON.test_create_flavor_with_int_id
-#testr run tempest.api.compute.admin.test_hypervisor.HypervisorAdminTestXML.test_get_hypervisor_uptime
-
+testr run --parallel --subunit  --load-list=/home/ubuntu/testr.list |  subunit-2to1 |  tools/colorizer.py > /home/ubuntu/tempest/tempest-output.log 2>&1
