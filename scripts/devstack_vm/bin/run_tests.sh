f1039526d5edf95331028fd615436a6fac1749bd
Gabriel Samfira <samfiragabriel@gmail.com>
2014-01-20 08:49:12 -0500

    exclude AttachInterfaces

--- scripts/devstack_vm/bin/run_tests.sh
+++ scripts/devstack_vm/bin/run_tests.sh
@@ -7,6 +7,6 @@ testr init
 mkdir -p /home/ubuntu/tempest
 
 #exclude unsupported tests
-testr list-tests tempest.api.compute | grep -v "rescue\|_uptime\|_console_" > /home/ubuntu/testr.list
+testr list-tests tempest.api.compute | grep -v "rescue\|_uptime\|_console_\|AttachInterfaces" > /home/ubuntu/testr.list
 
 testr run --parallel --subunit  --load-list=/home/ubuntu/testr.list |  subunit-2to1 |  tools/colorizer.py > /home/ubuntu/tempest/tempest-output.log 2>&1
