c320a221010d274d8b5f5bb3f17b9f777dba1c33
Gabriel <samfiragabriel@gmail.com>
2014-02-04 10:00:40 +0200

    redirect console output to file

--- scripts/HyperV/scripts/run_openstack_service.bat
+++ scripts/HyperV/scripts/run_openstack_service.bat
@@ -2,5 +2,6 @@
 
 set JOB=%1
 set CONF=%2
+set CONSOLELOG=%3
 
-C:\OpenStack\virtualenv\Scripts\activate.bat && %JOB% --config-file=%CONF%
+C:\OpenStack\virtualenv\Scripts\activate.bat && %JOB% --config-file=%CONF% > %CONSOLELOG% 2>&1
