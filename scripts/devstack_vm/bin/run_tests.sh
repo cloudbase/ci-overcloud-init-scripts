62192a6abbf39d204a54c5c4abdea604582c5ded
Gabriel Samfira <samfiragabriel@gmail.com>
2014-02-04 09:06:14 -0500

    readded exclude list

--- scripts/devstack_vm/bin/run_tests.sh
+++ scripts/devstack_vm/bin/run_tests.sh
@@ -6,13 +6,16 @@ testr init
 
 TEMPEST_DIR="/home/ubuntu/tempest"
 EXCLUDED_TESTS="$TEMPEST_DIR/excluded_tests.txt"
-
+RUN_TESTS_LIST="$TEMPEST_DIR/test_list.txt"
 mkdir -p "$TEMPEST_DIR"
 
-# make a list of excluded tests
+# make a list of excluded tests. Informative
 testr list-tests tempest.api.compute | grep "rescue\|_uptime\|_console_\|AttachInterfaces" > "$EXCLUDED_TESTS" || echo "failed to generate exclude list"
 
-testr run --parallel --subunit  --load-list=$EXCLUDED_TESTS |  subunit-2to1  > /home/ubuntu/tempest/subunit-output.log 2>&1
+# Run tests list
+testr list-tests tempest.api.compute | grep -v "rescue\|_uptime\|_console_\|AttachInterfaces" > "$RUN_TESTS_LIST" || echo "failed to generate exclude list"
+
+testr run --parallel --subunit  --load-list=$RUN_TESTS_LIST |  subunit-2to1  > /home/ubuntu/tempest/subunit-output.log 2>&1
 cat /home/ubuntu/tempest/subunit-output.log | /opt/stack/tempest/tools/colorizer.py > /home/ubuntu/tempest/tempest-output.log 2>&1
 # testr exits with status 0. colorizer.py actually sets correct exit status
 RET=$?
