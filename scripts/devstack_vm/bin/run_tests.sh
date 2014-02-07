52d8a85af3c95f9b9679dd92b0551ceb33336683
Gabriel Adrian Samfira <gsamfira@cloudbasesolutions.com>
2014-02-07 19:45:36 +0200

    forgot double redirect

--- scripts/devstack_vm/bin/run_tests.sh
+++ scripts/devstack_vm/bin/run_tests.sh
@@ -20,7 +20,7 @@ test_for_nova (){
     echo '# Unimplemented' >> "$EXCLUDED_TESTS"
     testr list-tests tempest | grep "rescue\|_uptime\|_console_\|AttachInterfaces" >> "$EXCLUDED_TESTS" || echo "failed to generate exclude list"
     echo '# AMI images not supported' >> "$EXCLUDED_TESTS"
-    testr list-tests tempest | grep "TestMinimumBasicScenario" > "$EXCLUDED_TESTS" || echo "failed to generate exclude list"
+    testr list-tests tempest | grep "TestMinimumBasicScenario" >> "$EXCLUDED_TESTS" || echo "failed to generate exclude list"
     # Run tests list
     testr list-tests tempest.api.compute | grep -v "TestMinimumBasicScenario\|rescue\|_uptime\|_console_\|AttachInterfaces" > "$RUN_TESTS_LIST" || echo "failed to generate exclude list"    
 }
