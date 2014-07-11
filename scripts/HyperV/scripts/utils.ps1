42a3defb490f03b638bb987af886c876f7442c3d
Nesa Ion-Claudiu <claudiu.nesa@gmail.com>
2014-07-11 16:17:47 +0300

    Added utils script

---
+++ scripts/HyperV/scripts/utils.ps1
@@ -0,0 +1,72 @@
+function ExecRetry($command, $maxRetryCount = 10, $retryInterval=2)
+{
+    $currErrorActionPreference = $ErrorActionPreference
+    $ErrorActionPreference = "Continue"
+
+    $retryCount = 0
+    while ($true)
+    {
+        try 
+        {
+            & $command
+            break
+        }
+        catch [System.Exception]
+        {
+            $retryCount++
+            if ($retryCount -ge $maxRetryCount)
+            {
+                $ErrorActionPreference = $currErrorActionPreference
+                throw
+            }
+            else
+            {
+                Write-Error $_.Exception
+                Start-Sleep $retryInterval
+            }
+        }
+    }
+
+    $ErrorActionPreference = $currErrorActionPreference
+}
+
+function GitClonePull($path, $url, $branch="master")
+{
+    if (!(Test-Path -path $path))
+    {
+        ExecRetry {
+            git clone $url $path
+            if ($LastExitCode) { throw "git clone failed" }
+        }
+    }else{
+        pushd $path
+        try
+        {
+            if($branch)
+            {
+                ExecRetry {
+                    git checkout $branch
+                    if ($LastExitCode) { throw "git checkout failed" }
+                }
+            }
+
+            Get-ChildItem . -Include *.pyc -Recurse | foreach ($_) {Remove-Item $_.fullname}
+
+
+            git reset --hard
+            if ($LastExitCode) { throw "git reset failed" }
+
+            git clean -f -d
+            if ($LastExitCode) { throw "git clean failed" }
+
+            ExecRetry {
+                git pull
+                if ($LastExitCode) { throw "git pull failed" }
+            }
+        }
+        finally
+        {
+            popd
+        }
+    }
+}
