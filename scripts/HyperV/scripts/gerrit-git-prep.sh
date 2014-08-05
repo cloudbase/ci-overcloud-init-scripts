2dabefbd6198e82dab0527709cde2a18f2ed64ba
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2014-08-05 17:09:31 +0300

    updated version to match devstack one

--- scripts/HyperV/scripts/gerrit-git-prep.sh
+++ scripts/HyperV/scripts/gerrit-git-prep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 function usage() {
-	echo "$0 --zuul-site ZUUL_SITE --gerrit-site GERRIT_SITE --zuul-ref ZUUL_REF --zuul-change ZUUL_CHANGE --zuul-project ZUUL_PROJECT [--git-origin GIT_ORIGIN] [--zuul-newrev ZUUL_NEWREV]"
+    echo "$0 --zuul-site ZUUL_SITE --gerrit-site GERRIT_SITE --zuul-ref ZUUL_REF --zuul-change ZUUL_CHANGE --zuul-project ZUUL_PROJECT [--git-origin GIT_ORIGIN] [--zuul-newrev ZUUL_NEWREV]"
 }
 
 while [ $# -gt 0 ];
@@ -13,37 +13,37 @@ do
         --gerrit-site)
             GERRIT_SITE=$2
             shift;;
-		--git-origin)
-			GIT_ORIGIN=$2
-			shift;;
-		--zuul-newrev)
-			ZUUL_NEWREV=$2
-			shift;;
-		--zuul-ref)
-			ZUUL_REF=$2
-			shift;;
-		--zuul-change)
-			ZUUL_CHANGE=$2
-			shift;;
-		--zuul-project)
-			ZUUL_PROJECT=$2
-			shift;;
+    --git-origin)
+        GIT_ORIGIN=$2
+        shift;;
+    --zuul-newrev)
+        ZUUL_NEWREV=$2
+        shift;;
+    --zuul-ref)
+        ZUUL_REF=$2
+        shift;;
+    --zuul-change)
+        ZUUL_CHANGE=$2
+        shift;;
+    --zuul-project)
+        ZUUL_PROJECT=$2
+        shift;;
     esac
     shift
 done
 
 if [ -z "$ZUUL_REF" ] || [ -z "$ZUUL_CHANGE" ] || [ -z "$ZUUL_PROJECT" ]
 then
-	echo "ZUUL_REF ZUUL_CHANGE ZUUL_PROJECT are mandatory"
-	exit 1
+    echo "ZUUL_REF ZUUL_CHANGE ZUUL_PROJECT are mandatory"
+    exit 1
 fi
 
 BUILD_DIR="c:/OpenStack/build/"
 PROJECT_DIR="$BUILD_DIR/$ZUUL_PROJECT"
 
 function exit_error(){
-	echo $1
-	exit 1
+    echo $1
+    exit 1
 }
 
 if [ -z "$GERRIT_SITE" ]
@@ -76,22 +76,33 @@ then
     echo "Triggered by: $GERRIT_SITE/$ZUUL_CHANGE"
 fi
 
-set -x
-
-if [ ! -d "$BUILD_DIR" ]
+if [ -d "$BUILD_DIR" ]
 then
-	mkdir -p "$BUILD_DIR" || exit_error "Failed to create build dir"
+  rm -rf "$BUILD_DIR"
 fi
+mkdir -p "$BUILD_DIR" || exit_error "Failed to create build dir"
 
-if [ ! -d "$PROJECT_DIR" ]
+if [ -d "$PROJECT_DIR" ]
 then
-	mkdir -p  "$PROJECT_DIR" || exit_error "Failed to create project dir"
+  rm -rf "$PROJECT_DIR"
 fi
+mkdir -p  "$PROJECT_DIR" || exit_error "Failed to create project dir"
 
 cd "$PROJECT_DIR" || exit_error "Failed to enter project build dir"
 
-rm -rf "$PROJECT_DIR/*"
-git clone $GIT_ORIGIN/$ZUUL_PROJECT .
+set -x
+
+if [[ ! -e .git ]]
+then
+    ls -a
+    rm -fr .[^.]* *
+    if [ -d /opt/git/$ZUUL_PROJECT/.git ]
+    then
+        git clone file:///opt/git/$ZUUL_PROJECT .
+    else
+        git clone $GIT_ORIGIN/$ZUUL_PROJECT .
+    fi
+fi
 git remote set-url origin $GIT_ORIGIN/$ZUUL_PROJECT
 
 # attempt to work around bugs 925790 and 1229352
@@ -108,22 +119,30 @@ if ! git clean -x -f -d -q ; then
     git clean -x -f -d -q
 fi
 
-if [ -z "$ZUUL_NEWREV" ]
+echo "Before doing git checkout:"
+echo "Git branch output:"
+git branch
+echo "Git log output:"
+git log -10 --pretty=format:"%h - %an, %ae, %ar : %s"
+
+if echo "$ZUUL_REF" | grep -q ^refs/tags/
+then
+    git fetch --tags $ZUUL_URL/$ZUUL_PROJECT
+    git checkout $ZUUL_REF
+    git reset --hard $ZUUL_REF
+elif [ -z "$ZUUL_NEWREV" ]
 then
     git fetch $ZUUL_SITE/p/$ZUUL_PROJECT $ZUUL_REF
     git checkout FETCH_HEAD
     git reset --hard FETCH_HEAD
-    if ! git clean -x -f -d -q ; then
-        sleep 1
-        git clean -x -f -d -q
-    fi
 else
     git checkout $ZUUL_NEWREV
     git reset --hard $ZUUL_NEWREV
-    if ! git clean -x -f -d -q ; then
-        sleep 1
-        git clean -x -f -d -q
-    fi
+fi
+
+if ! git clean -x -f -d -q ; then
+    sleep 1
+    git clean -x -f -d -q
 fi
 
 if [ -f .gitmodules ]
@@ -132,3 +151,9 @@ then
     git submodule sync
     git submodule update --init
 fi
+
+echo "Final result:"
+echo "Git branch output:"
+git branch
+echo "Git log output:"
+git log -10 --pretty=format:"%h - %an, %ae, %ar : %s"
