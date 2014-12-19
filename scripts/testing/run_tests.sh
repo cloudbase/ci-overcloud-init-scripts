f5646baa3cdbc34d4d44b1c845fdc58a273e8cb2
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2014-12-19 16:43:03 +0200

    automatically transfer logs to controller

--- scripts/testing/run_tests.sh
+++ scripts/testing/run_tests.sh
@@ -3,6 +3,7 @@
 WORK_DIR=`dirname $0`
 source $HOME/octavian/data/run_params.txt
 CONSOLE_LOG="$HOME/octavian/data/console-$NAME.log"
+LOGS_ROOT="$HOME/octavian/data/"
 
 #Import library functions
 source $WORK_DIR/../devstack_vm/bin/library.sh
@@ -25,3 +26,10 @@ PROJECT_NAME=$(basename $PROJECT)
 tempest_dir="/opt/stack/tempest"
 run_ssh_cmd ubuntu@$FLOATING_IP /home/jenkins-slave/admin-msft.pem "mkdir -p $tempest_dir" >> $CONSOLE_LOG 2>&1
 run_ssh_cmd ubuntu@$FLOATING_IP /home/jenkins-slave/admin-msft.pem "source /home/ubuntu/keystonerc; /home/ubuntu/bin/run-all-tests.sh $tempest_dir" >> $CONSOLE_LOG 2>&1
+LOGS_FOLDER=$LOGS_ROOT`date +%Y-%m-%d-%H%M%S`
+mkdir $LOGS_FOLDER
+scp -o 'PasswordAuthentication no' -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile /dev/null' -i /home/jenkins-slave/admin-msft.pem ubuntu@$FLOATING_IP:/home/ubuntu/bin/results.html $LOGS_FOLDER
+scp -o 'PasswordAuthentication no' -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile /dev/null' -i /home/jenkins-slave/admin-msft.pem ubuntu@$FLOATING_IP:/home/ubuntu/bin/subunit-output.log $LOGS_FOLDER
+scp -o 'PasswordAuthentication no' -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile /dev/null' -i /home/jenkins-slave/admin-msft.pem ubuntu@$FLOATING_IP:/opt/stack/logs/stack.sh.txt $LOGS_FOLDER
+scp -o 'PasswordAuthentication no' -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile /dev/null' -i /home/jenkins-slave/admin-msft.pem ubuntu@$FLOATING_IP:/opt/stack/logs/screen/* $LOGS_FOLDER
+rm -rf $LOGS_FOLDER/*`date +%Y-%m`*
