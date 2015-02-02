d0590dfc54dc50efc045a989e4c55ec198e4aec9
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-02-02 18:01:41 +0200

    Fix due to pbr / exe file bug

--- scripts/HyperV/scripts/run_openstack_service.bat
+++ scripts/HyperV/scripts/run_openstack_service.bat
@@ -1,7 +1,8 @@
 @echo off
 
-set JOB=%1
-set CONF=%2
-set CONSOLELOG=%3
+set PY_EXEC=%1
+set PY_FILE=%2
+set CONF=%3
+set CONSOLELOG=%4
 
-C:\OpenStack\virtualenv\Scripts\activate.bat && %JOB% --config-file=%CONF% > %CONSOLELOG% 2>&1
+C:\OpenStack\virtualenv\Scripts\activate.bat && %PY_EXEC% %PY_FILE% --config-file=%CONF% > %CONSOLELOG% 2>&1
