91a4cba7977967b0a0635e02152228b0616d882f
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-02-19 02:16:16 +0200

    remove pip install networking-hyperv

--- scripts/HyperV/scripts/install_openstack_from_repo.bat
+++ scripts/HyperV/scripts/install_openstack_from_repo.bat
@@ -1,5 +1,5 @@
 @echo off
 set PROJ=%1
-C:\OpenStack\virtualenv\Scripts\activate.bat && pip install -U networking-hyper-v --pre && cd %PROJ% && python setup.py install
+C:\OpenStack\virtualenv\Scripts\activate.bat && cd %PROJ% && python setup.py install
 
 
