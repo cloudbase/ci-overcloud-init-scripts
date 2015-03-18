af778de5fcd009be8a9ad6dcc237b6d2d0018a5c
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-03-18 10:41:00 +0200

    Fixed bug in fetching subnet_id
    Should not rely on line number for such operation.

--- scripts/devstack_vm/devstack/local.sh
+++ scripts/devstack_vm/devstack/local.sh
@@ -14,7 +14,7 @@ nova flavor-delete 451
 nova flavor-create m1.heat 451 512 5 1
 
 # Add DNS config to the private network
-subnet_id=`neutron net-show private | awk '{if (NR == 13) { print $4}}'`
+subnet_id=`neutron net-show private | grep subnets | awk '{print $4}'`
 neutron subnet-update $subnet_id --dns_nameservers list=true 8.8.8.8 8.8.4.4
 
 TEMPEST_CONFIG=/opt/stack/tempest/etc/tempest.conf
