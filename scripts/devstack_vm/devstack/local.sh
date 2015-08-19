172452044eff528f87fde0086d9f96057c2415ee
Octavian Ciuhandu <ociuhandu@cloudbasesolutions.com>
2015-08-20 01:50:52 +0300

    Add flavor and network debug info

--- scripts/devstack_vm/devstack/local.sh
+++ scripts/devstack_vm/devstack/local.sh
@@ -4,6 +4,9 @@ set -e
 source /home/ubuntu/devstack/functions
 source /home/ubuntu/devstack/functions-common
 
+echo "Before updating nova flavors:"
+nova flavor-list
+
 nova flavor-delete 42
 nova flavor-create m1.nano 42 96 1 1
 
@@ -13,9 +16,19 @@ nova flavor-create m1.micro 84 128 2 1
 nova flavor-delete 451
 nova flavor-create m1.heat 451 512 5 1
 
+echo "After updating nova flavors:"
+nova flavor-list
+
 # Add DNS config to the private network
 subnet_id=`neutron net-show private | grep subnets | awk '{print $4}'`
 neutron subnet-update $subnet_id --dns_nameservers list=true 8.8.8.8 8.8.4.4
+
+echo "Neutron networks:"
+neutron net-list
+for net in `neutron net-list | grep -v '\-\-' | grep -v "subnets" | awk {'print $2'}`; do neutron net-show $net; done
+echo "Neutron subnetworks:"
+neutron subnet-list
+for subnet in `neutron subnet-list | grep start | awk {'print $2'}`; do neutron subnet-show $subnet; done
 
 TEMPEST_CONFIG=/opt/stack/tempest/etc/tempest.conf
 
